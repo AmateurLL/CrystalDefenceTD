718415e526d642f989f8d9004babb10e2766ac72
AmateurLL <Amateurlslnz@gmail.com>
2020-02-27 16:09:26 +1300

    First Day Push
    + Crystal Prototype added
    + Prototype Tower added
    + Prototype Enemy added

---
+++ Cyber Defence/Assets/Scripts/Prototype/ProtoEnemy.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ProtoEnemy : MonoBehaviour
+{
+    [Header("Components")]
+    public Rigidbody m_Rigbody;
+
+    public GameObject m_TestTarget;
+
+    [Space]
+    [Header("Stats")]
+    public float health = 10.0f;
+    public float movementSpeed = 5.0f;
+
+
+    [Space]
+    [Header("Misc")]
+    public float distToCrystal = 0.0f;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        m_Rigbody = GetComponent<Rigidbody>();
+        
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        CheckDistanceToCrystal();
+
+
+    }
+
+    void CheckDistanceToCrystal(){
+
+        distToCrystal = Vector3.Distance(m_TestTarget.transform.position, this.transform.position);
+        Debug.Log("Crystal Distance : " + distToCrystal);
+    }
+
+
+
+}
---
+++ Cyber Defence/Assets/Scripts/Prototype/ProtoTower.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ProtoTower : MonoBehaviour
+{
+
+    [Header("Components")]
+    public BoxCollider m_BodyColli;
+    public SphereCollider m_AttackRangeColli;
+
+    [Space]
+    [Header("Stats")]
+    public float health = 15.0f;
+    public float attackRange = 12.0f;
+    public float attackSpeed = 8.0f;
+    public float attackCharge = 0.0f;
+
+    public GameObject mobTarget;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        m_AttackRangeColli = GetComponent<SphereCollider>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void Attacking(){
+
+        attackCharge += Time.deltaTime;
+        if(attackCharge >= attackSpeed){
+            Debug.Log("Shots Fired");
+            attackCharge = 0;
+        }
+    }
+
+    void OnTriggerEnter(Collider targetsInRange){
+
+
+    }
+
+    void EnemyInRange(){
+        // Check for Mobs with attackrange components sphere collider
+        //Colliders[] inColliders = m_AttackRangeColli();
+
+
+
+    }
+
+}
